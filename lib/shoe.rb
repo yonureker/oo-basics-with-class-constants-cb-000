a994d8e458be34c432eefa927eb24dc24cbd3462
Onur Eker <yonureker@gmail.com>
2018-01-03 00:11:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.all? {|x| x != bread} == true
+    if BRANDS.all? {|x| x != brand} == true
       BRANDS << brand
     end
     end
